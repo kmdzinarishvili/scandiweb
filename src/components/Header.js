9a7288877eee6fae6fc9dc63ce1838a91eb36f22
kmdzinarishvili <ketevan.mdzinarishvili.1@btu.edu.ge>
2022-08-20 17:00:47 +0400

    adding form

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,15 @@ const Header = ({ pageName, button1, button2 }) => {
     return <div className='header'>
         <h2 className='title'>Project {pageName}</h2>
         <span className='buttons'>
-            <button onClick={button1.function} >{button1.text}</button>
-            <button onClick={button2.function} id={button2.id ? button2.id : null}>{button2.text} </button>
+            <button onClick={button1.function}
+                form={button1.form ? button1.form : null}
+                type={button1.form ? "submit" : "button"}>
+                {button1.text}
+            </button>
+            <button onClick={button2.function}
+                id={button2.id ? button2.id : null}>
+                {button2.text}
+            </button>
         </span>
     </div>
 }
--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+import "../styles/add.css";
+
 
 
 const Add = () => {
+    const [inputValues, setInputValue] = useState({
+        sku: "",
+        name: "",
+        price: "",
+        productType: "DVD",
+
+    });
+
+    //handle submit updates
+
     const navigate = useNavigate();
 
     const goToHome = () => {
@@ -13,22 +26,64 @@ const Add = () => {
     }
     const saveBtn = {
         "text": "save",
-        "function": () => console.log("save"),
-        "id": "save-product-btn"
+        "function": null,
+        "form": 'product_form'
     }
 
     const cancelBtn = {
         "text": "cancel",
         "function": () => goToHome(),
-        // "id": 'cancel-btn'
     }
 
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setInputValue({ ...inputValues, [name]: value });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        navigate('/');
+    }
     return <> <Header pageName="Add"
         button1={saveBtn}
         button2={cancelBtn} />
-        <div className="container">
+        <form id="product_form" onSubmit={handleSubmit}>
+            <label>
+                SKU:
+                <input type="text"
+                    name="sku"
+                    id='sku'
+                    value={inputValues.sku}
+                    onChange={(e) => handleChange(e)} />
+            </label>
 
-        </div>
+            <label>
+                Name:
+                <input
+                    type="text"
+                    name="name"
+                    id='name'
+                    value={inputValues.name}
+                    onChange={(e) => handleChange(e)} />
+            </label>
+            <label>
+                Price:
+                <input type="text"
+                    name="price"
+                    id='price'
+                    value={inputValues.price}
+                    onChange={(e) => handleChange(e)} />
+            </label>
+            <label>
+                Type Switcher:
+                <select id="productType" name="productType"
+                    onChange={(e) => handleChange(e)}>
+                    <option value="DVD">DVD</option>
+                    <option value="Book">Book</option>
+                    <option value="Furniture">Furniture</option>
+                </select>
+            </label>
+        </form>
 
         <Footer />
     </>
